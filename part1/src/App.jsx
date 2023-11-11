2b2d2653775173a1c9b3a8b11d72f19ff8b7dc4d
KarenMkrtchyan <70455623+KarenMkrtchyan@users.noreply.github.com>
2023-11-11 15:19:49 -0800

    god is dead

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,61 +1,27 @@
-const Header = (props) => {
-	return <h1>{props.course}</h1>;
+import { useState } from "react";
+
+const Display = (props) => {
+	return <div>{props.counter}</div>;
 };
 
-const Part = (props) => {
-	return (
-		<p>
-			{props.name} {props.exercises}
-		</p>
-	);
-};
-
-const Content = (props) => {
-	return (
-		<div>
-			<Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
-			<Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
-			<Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
-		</div>
-	);
-};
-
-const Total = (props) => {
-	return (
-		<p>
-			Number of exercises{" "}
-			{props.total[0].exercises +
-				props.total[1].exercises +
-				props.total[2].exercises}
-		</p>
-	);
+const Button = (props) => {
+	return <button onClick={props.onClick}>{props.text}</button>;
 };
 
 const App = () => {
-	const course = {
-		name: "Half Stack application development",
-		parts: [
-			{
-				name: "Fundamentals of React",
-				exercises: 10,
-			},
-			{
-				name: "Using props to pass data",
-				exercises: 7,
-			},
-			{
-				name: "State of a component",
-				exercises: 14,
-			},
-		],
-	};
+	const [counter, setCounter] = useState(0);
+
+	const increaseByOne = () => setCounter(counter + 1);
+	const decreaseByOne = () => setCounter(counter - 1);
+	const setToZero = () => setCounter(0);
 
 	return (
 		<div>
-			<Header course={course.name} />
-			<Content parts={course.parts} />
-			<Total total={course.parts} />
+			<Display counter={counter} />
+			<Button onClick={increaseByOne} text="plus" />
+			<Button onClick={decreaseByOne} text="minus" />
 		</div>
 	);
 };
+
 export default App;
