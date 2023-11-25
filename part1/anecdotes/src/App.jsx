66838c68c36cc4e337cfe2fab4a801cef7723a61
KarenMkrtchyan <70455623+KarenMkrtchyan@users.noreply.github.com>
2023-11-24 23:09:53 -0800

    no work :(

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+const App = () => {
+	const anecdotes = [
+		"If it hurts, do it more often.",
+		"Adding manpower to a late software project makes it later!",
+		"The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+		"Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+		"Premature optimization is the root of all evil.",
+		"Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+		"Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+		"The only way to go fast, is to go well.",
+	];
+	const [points, setPoints] = useState(Array(anecdotes.length).fill(0));
+	const [selected, setSelected] = useState(0);
+	const [mostVotes, setMostVotes] = useState(0);
+	const handleNext = () => {
+		setSelected(Math.floor(Math.random() * anecdotes.length));
+	};
+
+	const handleVote = () => {
+		const copy = points.slice();
+		copy[selected] += 1;
+		setPoints(copy);
+		let max = 0;
+		for (let i = 0; i < points.length; i++) {
+			if (points[i] > max) {
+				max = i;
+			}
+		}
+		setMostVotes(max);
+		console.log(anecdotes[mostVotes]);
+	};
+
+	return (
+		<div>
+			{anecdotes[selected]}
+			<div>had {points[selected]} votes</div>
+			<div>
+				<button onClick={handleVote}>vote</button>
+				<button onClick={handleNext}>next anecdote</button>
+			</div>
+			<div>
+				<h1>Anecdote with most votes</h1>
+				<div>{anecdotes[mostVotes]}</div>
+			</div>
+		</div>
+	);
+};
+
+export default App;
